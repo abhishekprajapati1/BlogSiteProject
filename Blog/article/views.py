ce23f6465334d20848c0aff57d0cd78c5b3536cd
abhi0661236 <ap0661236@gmail.com>
2021-02-21 11:46:39 +0530

    A tiny update

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def bloghome(request):
-    return HttpResponse("This is blog page we will keep all the blog post here")
+    return render(request, 'article/home.html')
+    # return HttpResponse("This is blog page we will keep all the blog post here")
 
 
 def blogpost(request, slug):
+    return render(request, 'article/post.html')
     return HttpResponse(f"This is blogpost page for {slug}")
--- Blog/home/views.py
+++ Blog/home/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is home page")
+    return render(request, 'home/home.html')
+    # return HttpResponse("This is home page")
 
 def contact(request):
-    return HttpResponse("This is contact page")
+    return render(request, 'home/contact.html')
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, 'home/about.html')
