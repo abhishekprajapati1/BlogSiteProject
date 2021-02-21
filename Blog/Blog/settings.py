daeaf672821f31d8fd562f8bf2883dda5d0d055d
abhi0661236 <ap0661236@gmail.com>
2021-02-21 14:05:28 +0530

    Added a contact form

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'article.apps.ArticleConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'static',
+]
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
--- Blog/home/views.py
+++ Blog/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,6 +9,19 @@ def home(request):
     # return HttpResponse("This is home page")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        msg = request.POST['desc']
+        print(name,email,phone,msg)
+
+        if len(name) < 2 or len(email) < 5 or len(phone) < 10 or len(msg) < 5:
+            messages.error(request, 'Please fill the form correctly')
+        else:
+            contact = Contact(name=name, phone=phone, email=email, content=msg)
+            contact.save()
+            messages.success(request, 'Sent successfully')
     return render(request, 'home/contact.html')
 
 def about(request):
